ca6d5b884887f754ccf573a5186bf843e3548238
Tobias Ves Losada <tveslosada@itba.edu.ar>
2026-06-01 10:58:37 -0300

    style: change average rating label to 'Prom. votos'

--- src/components/session/MysteryVoteWidget.tsx
+++ src/components/session/MysteryVoteWidget.tsx
@@ -244,7 +244,7 @@ export default function MysteryVoteWidget() {
                   }}
                   className="whitespace-nowrap"
                 >
-                  Promedio: {averageGiven}
+                  Prom. votos: {averageGiven}
                 </span>
               </div>
             </div>
